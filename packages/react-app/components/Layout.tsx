d178750a70bc7428f7274c73b60ef9a9a3f47708
Njabulo <njabulomashu@gmail.com>
2024-06-22 14:36:14 +0200

    Fix d

--- packages/react-app/components/Layout.tsx
+++ packages/react-app/components/Layout.tsx
@@ -15,9 +15,10 @@ const Layout: FC<Props> = ({ children, backgroundColor, contentAreaBackgroundCol
       >
         <Header />
         <main
-          className={`content-area py-16 max-w-7xl mx-auto space-y-8 sm:px-6 lg:px-8 ${
+          className={`content-area py-16 max-w-7xl mx-auto space-y-8 sm:px-6 lg:px-8 bg-pink-500 ${
             contentAreaBackgroundColor ? `bg-${contentAreaBackgroundColor}` : ''
           }`}
+          
         >
           {children}
         </main>
